842032477108e4c807b7a0af60ea6b667e7e54f5
NAGASRISWETHA22 <2k22it31@kiot.ac.in>
2026-02-20 14:22:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let timer;
+let startTime;
+let elapsedTime = 0;
+let isRunning = false;
+
+const display = document.getElementById('display');
+const startBtn = document.getElementById('startBtn');
+const resetBtn = document.getElementById('resetBtn');
+const lapBtn = document.getElementById('lapBtn');
+const lapsList = document.getElementById('lapsList');
+
+function formatTime(ms) {
+    let milliseconds = Math.floor((ms % 1000) / 10);
+    let seconds = Math.floor((ms / 1000) % 60);
+    let minutes = Math.floor((ms / (1000 * 60)) % 60);
+    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+    return (
+        (hours < 10 ? "0" + hours : hours) + ":" +
+        (minutes < 10 ? "0" + minutes : minutes) + ":" +
+        (seconds < 10 ? "0" + seconds : seconds) + "." +
+        (milliseconds < 10 ? "0" + milliseconds : milliseconds)
+    );
+}
+
+startBtn.addEventListener('click', () => {
+    if (!isRunning) {
+      isRunning = true;
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(() => {
+            elapsedTime = Date.now() - startTime;
+            display.innerText = formatTime(elapsedTime);
+        }, 10);
+        startBtn.innerText = "Pause";
+        startBtn.style.backgroundColor = "#ff4d4d"; 
+    } else {
+       
+        isRunning = false;
+        clearInterval(timer);
+        startBtn.innerText = "Resume";
+        startBtn.style.backgroundColor = "#6c63ff";
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    isRunning = false;
+    elapsedTime = 0;
+    display.innerText = "00:00:00.00";
+    startBtn.innerText = "Start";
+    startBtn.style.backgroundColor = "#6c63ff";
+    lapsList.innerHTML = ""; 
+});
+
+lapBtn.addEventListener('click', () => {
+    if (isRunning) {
+        const lapTime = formatTime(elapsedTime);
+        const li = document.createElement('li');
+        li.innerHTML = `<span>Lap ${lapsList.children.length + 1}</span> <span>${lapTime}</span>`;
+        lapsList.prepend(li); 
+    }
+});
