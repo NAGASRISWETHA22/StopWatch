33e47ec24c1df05e6fcd5bd2729d6261948c1262
NAGASRISWETHA22 <152147769+NAGASRISWETHA22@users.noreply.github.com>
2026-03-01 11:24:32 +0530

    updated time formatting
    Removed hours calculation from formatTime function.

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ function formatTime(ms) {
     let milliseconds = Math.floor((ms % 1000) / 10);
     let seconds = Math.floor((ms / 1000) % 60);
     let minutes = Math.floor((ms / (1000 * 60)) % 60);
-    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
-
+   
     return (
-        (hours < 10 ? "0" + hours : hours) + ":" +
         (minutes < 10 ? "0" + minutes : minutes) + ":" +
         (seconds < 10 ? "0" + seconds : seconds) + "." +
         (milliseconds < 10 ? "0" + milliseconds : milliseconds)
